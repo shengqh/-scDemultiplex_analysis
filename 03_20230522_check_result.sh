a6fc4e8863ac48f5533524dfcf5ce0aca726de5b
Quanhu Sheng <shengqh@gmail.com>
2023-05-26 22:56:53 -0500

    to 20230526

--- 03_20230522_check_result.sh
+++ 03_20230522_check_result.sh
@@ -1,4 +1,4 @@
 cd /nobackup/h_cqs/collaboration/20230522_scrna_hto
 
-cp -f /home/shengq2/program/scDemultiplex_analysis/03_20230522_check_result.rmd 20230525_check_result.rmd
-R -e 'rmarkdown::render("20230525_check_result.rmd")'
+cp -f /home/shengq2/program/scDemultiplex_analysis/03_20230522_check_result.rmd 20230526_check_result.rmd
+R -e 'rmarkdown::render("20230526_check_result.rmd")'
